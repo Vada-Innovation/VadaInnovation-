e6ee7bf55de90500f0f29dffe4480eb91bedbd7a
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-03-30 20:03:56 +0500

    Added info

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home = () => {
         >
           <div className="w-10/12 md:w-8/12 text-center">
             <PageSentence
-              title="We Design. We Develop. We Ship. In The Same Day."
+              title="We Deliver what we promise."
               description="We are committed to not making clients wait. We will deliver the work as quickly as possible. Even on the same day. Even so, we do not reduce the quality of our work."
               badge="CLIENT-DEVELOPMENT-DRIVEN"
             />
