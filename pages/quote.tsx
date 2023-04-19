0e5c79614f15514df069d4bcf1e6ea2068242972
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-04-19 10:25:04 +0500

    added snackbar

--- pages/quote.tsx
+++ pages/quote.tsx
@@ -9,6 +9,9 @@ import TextAreaGroup from 'components/molecules/FormGroup/TextAreaGroup';
 import PageSentence from 'components/molecules/PageSentence';
 import PageTemplate from 'components/templates/PageTemplate';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Quote = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -32,25 +35,55 @@ const Quote = () => {
     // Add any additional logic for form submission here
 
     // console.log('Name: ', name, email, phoneNumber, yourProblem);
+    console.log('name: ',name, 'email: ', email, 'phoneNumber: ', phoneNumber, 'yourProblem: ', yourProblem)
 
+    if(name === '' || email === '' || phoneNumber === '' || yourProblem === '') {
+      toast.error('Please fill all the fields', {
+        position: 'bottom-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    })
+  }
+  else{
     axios.post('https://eol2lhcifrly7ly.m.pipedream.net', {
       name: name,
       email: email,
       phoneNumber: phoneNumber,
       yourProblem: yourProblem,
-    }).then((response) => {
-      // console.log(response);
+    }).then(() => {
+        toast.success('Thank you! Your quote has been sent', {
+        position: 'bottom-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }).catch((error) => {
-      // console.log(error);
+      toast.error('Something went wrong', {
+        position: 'bottom-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    })
     });
     setName('');
     setEmail('');
     setPhoneNumber('');
     setYourProblem('');
-  }
-;
+  } 
+  };
   return (
     <>
+      <ToastContainer />
       <PageTemplate title='Send Quote - Vada Innovation'>
         <section className="grid grid-cols-1 place-items-center gap-5 lg:grid-cols-2">
           <aside className="w-full sm:w-10/12 md:w-8/12 grid grid-cols-1 place-items-center gap-12 lg:w-full lg:place-items-start" data-aos="fade-down-right">
@@ -74,7 +107,7 @@ const Quote = () => {
             <div className="grid grid-cols-1 gap-7 p-6 md:p-9 bg-light rounded-md lg:w-10/12 ">
               <div className="grid grid-cols-2 gap-4">
                 <InputGroup label="Name" value={name} onChange={(e) => setName(e.target.value)} />
-                <InputGroup label="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <InputGroup label="Email" type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
               </div>
               <div className="grid grid-cols-1 gap-4">
                 <InputGroup label="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
